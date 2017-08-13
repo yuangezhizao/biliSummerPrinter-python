375b91ca52655407161bef37c603932b3b7902c9
Z4HD <Z4HD@outlook.com>
2017-08-14 07:51:36 +0800

    添加DEMO脚本

---
+++ DEMO.py
@@ -0,0 +1,73 @@
+from biliSummerPrinter import biliSummerPrinter, BiliBitmap
+import time
+import progressbar  # pip install progressbar2
+import random
+import json
+
+
+def Timmer(cd):
+    bar = progressbar.ProgressBar()
+    for i in bar(range(cd)):
+        time.sleep(1)
+
+
+account = {}
+with open('account-test.json', 'rb') as f:
+    rb = f.read().decode('utf-8')
+    account = json.loads(rb)
+
+if account['cookie'] == "yourCookie":
+    print("[ERROR!]:谢绝默认迫真cookie")
+    input("按任意键退出程序>")
+    exit()
+
+printer = biliSummerPrinter(account['id'], account['cookie'])
+cd = printer.getCDTime()
+print("cdTime:%s" % cd)
+if cd != 0:
+    Timmer(cd)
+
+z4Map = object
+with open('z4hdMap.json', 'r') as target:
+    z4Map = BiliBitmap.decodeJSON(target.read())
+z4Map.printf()
+
+
+def checkBitmap(x,y):
+    # 参数为要检测的bitmap的左上角像素在整个绘板中的坐标
+    bili = printer.getBitmap()
+    bili.cut(x, y, z4Map.length, z4Map.width)
+    bili.printf()
+    i = 0
+    diff = []
+    while i < len(bili.bitMap):
+        i2 = 0
+        while i2 < len(bili.bitMap[i]):
+            if bili.bitMap[i][i2] != z4Map.bitMap[i][i2]:
+                # 此处取得的X、y均为相对坐标，应与基础坐标相加来得到绝对坐标。
+                diff.append(
+                    {'x': i2, 'y': i, 'rightColor': z4Map.bitMap[i][i2]})
+            i2 += 1
+        i += 1
+    print(diff)
+    return diff
+
+
+def main():
+    while True:
+        diff = checkBitmap(1165,238)
+        if diff != []:
+            target1 = random.choice(diff)
+            print("TARGET:" + str(target1))
+            printer.draw(1165 + int(target1['x']), 238 +
+                         int(target1['y']), target1['rightColor'])
+            time.sleep(1)
+            Timmer(printer.getCDTime())
+        else:
+            print("[info]:GodJob! Diff is Empty! Waiting 30s for next")
+            Timmer(30)
+
+
+if __name__ == '__main__':
+    main()
+    input("Stopped!....Press Any Key To EXIT")
